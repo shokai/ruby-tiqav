a4239012b0de00dac5e5f3753b5060783243cfe1
Sho Hashimoto <hashimoto@shokai.org>
2013-04-10 16:06:20 +0900

    move AlphaNum class into Tiqav namespace #4

--- lib/tiqav/alpha_num.rb
+++ lib/tiqav/alpha_num.rb
@@ -1,24 +1,20 @@
 
-class AlphaNum
-  def self.table
-    @@table ||= '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
-  end
+module Tiqav
+  class AlphaNum
+    def self.table
+      @@table ||= '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+    end
 
-  def self.encode(num)
-    num > 61 ? self.encode(num/62)+self.table[num%62] : self.table[num%62]
-  end
+    def self.encode(num)
+      num > 61 ? self.encode(num/62)+self.table[num%62] : self.table[num%62]
+    end
 
-  def self.decode(str)
-    arr = str.split('').map{|i| self.table.index i }
-    for i in 0...arr.size do
-      return arr[i] if i == arr.size-1
-      arr[i+1] += arr[i]*62
+    def self.decode(str)
+      arr = str.split('').map{|i| self.table.index i }
+      for i in 0...arr.size do
+        return arr[i] if i == arr.size-1
+        arr[i+1] += arr[i]*62
+      end
     end
   end
-end
-
-
-if __FILE__ == $0
-  puts AlphaNum.decode 'Ab'
-  puts AlphaNum.encode 365
 end
